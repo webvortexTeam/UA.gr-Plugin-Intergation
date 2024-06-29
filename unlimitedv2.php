3eb92ba273c4f0bc17c5004bda25a4861ffc3220
Esmiraldo <esmiraldojaubelli@gmail.com>
2024-06-29 15:35:55 +0300

    updater call to json

--- unlimitedv2.php
+++ unlimitedv2.php
@@ -4,6 +4,9 @@ Plugin Name: Unlimited Andrenaline V2
 Description: API Intergation
 Author: WebVortex
 Text Domain: unlimited-andrenaline-v2
+Version: 1.0
+Author URI: https://www.webvortex.org
+License: GPL
 */
 
 // Exit if accessed directly
@@ -19,6 +22,8 @@ if (!class_exists('Requests')) {
 define('MY_PLUGIN_DIR_PATH', untrailingslashit(plugin_dir_path(__FILE__)));
 
 require_once MY_PLUGIN_DIR_PATH . '/class-tgm-plugin-activation.php';
+//require_once MY_PLUGIN_DIR_PATH . '/unlimitedv2-update.php';
+
 require_once MY_PLUGIN_DIR_PATH . '/inc/admin-settings.php';
 
 add_action('tgmpa_register', 'unlimited_andrenaline_v2_register_required_plugins');
@@ -214,4 +219,161 @@ require_once MY_PLUGIN_DIR_PATH . '/includes/unlimitedv2-class-api-calls-structu
 
 require_once MY_PLUGIN_DIR_PATH . '/includes/unlimitedv2-class-log.php'; // Debug
 
-?>
+if( ! class_exists( 'vortexUpdateChecker' ) ) {
+
+	class vortexUpdateChecker{
+
+		public $plugin_slug;
+		public $version;
+		public $cache_key;
+		public $cache_allowed;
+
+		public function __construct() {
+
+			$this->plugin_slug = plugin_basename( __DIR__ );
+			$this->version = '1.0';
+			$this->cache_key = '9r3u104910hrj13r1309rh13r490149u12';
+			$this->cache_allowed = false;
+
+			add_filter( 'plugins_api', array( $this, 'info' ), 20, 3 );
+			add_filter( 'site_transient_update_plugins', array( $this, 'update' ) );
+			add_action( 'upgrader_process_complete', array( $this, 'purge' ), 10, 2 );
+
+		}
+
+		public function request(){
+
+			$remote = get_transient( $this->cache_key );
+
+			if( false === $remote || ! $this->cache_allowed ) {
+
+				$remote = wp_remote_get(
+					'https://bzn.gr/file.json',
+					array(
+						'timeout' => 10,
+						'headers' => array(
+							'Accept' => 'application/json'
+						)
+					)
+				);
+
+				if(
+					is_wp_error( $remote )
+					|| 200 !== wp_remote_retrieve_response_code( $remote )
+					|| empty( wp_remote_retrieve_body( $remote ) )
+				) {
+					return false;
+				}
+
+				set_transient( $this->cache_key, $remote, DAY_IN_SECONDS );
+
+			}
+
+			$remote = json_decode( wp_remote_retrieve_body( $remote ) );
+
+			return $remote;
+
+		}
+
+
+		function info( $res, $action, $args ) {
+
+			// print_r( $action );
+			// print_r( $args );
+
+			// do nothing if you're not getting plugin information right now
+			if( 'plugin_information' !== $action ) {
+				return $res;
+			}
+
+			// do nothing if it is not our plugin
+			if( $this->plugin_slug !== $args->slug ) {
+				return $res;
+			}
+
+			// get updates
+			$remote = $this->request();
+
+			if( ! $remote ) {
+				return $res;
+			}
+
+			$res = new stdClass();
+
+			$res->name = $remote->name;
+			$res->slug = $remote->slug;
+			$res->version = $remote->version;
+			$res->tested = $remote->tested;
+			$res->requires = $remote->requires;
+			$res->author = $remote->author;
+			$res->author_profile = $remote->author_profile;
+			$res->download_link = $remote->download_url;
+			$res->trunk = $remote->download_url;
+			$res->requires_php = $remote->requires_php;
+			$res->last_updated = $remote->last_updated;
+
+			$res->sections = array(
+				'description' => $remote->sections->description,
+				'installation' => $remote->sections->installation,
+				'changelog' => $remote->sections->changelog
+			);
+
+			if( ! empty( $remote->banners ) ) {
+				$res->banners = array(
+					'low' => $remote->banners->low,
+					'high' => $remote->banners->high
+				);
+			}
+
+			return $res;
+
+		}
+
+		public function update( $transient ) {
+
+			if ( empty($transient->checked ) ) {
+				return $transient;
+			}
+
+			$remote = $this->request();
+
+			if(
+				$remote
+				&& version_compare( $this->version, $remote->version, '<' )
+				&& version_compare( $remote->requires, get_bloginfo( 'version' ), '<=' )
+				&& version_compare( $remote->requires_php, PHP_VERSION, '<' )
+			) {
+				$res = new stdClass();
+				$res->slug = $this->plugin_slug;
+				$res->plugin = plugin_basename( __FILE__ ); // webvortex-update-plugin/webvortex-update-plugin.php
+				$res->new_version = $remote->version;
+				$res->tested = $remote->tested;
+				$res->package = $remote->download_url;
+
+				$transient->response[ $res->plugin ] = $res;
+
+	    }
+
+			return $transient;
+
+		}
+
+		public function purge( $upgrader, $options ){
+
+			if (
+				$this->cache_allowed
+				&& 'update' === $options['action']
+				&& 'plugin' === $options[ 'type' ]
+			) {
+				// just clean the cache when new plugin version is installed
+				delete_transient( $this->cache_key );
+			}
+
+		}
+
+
+	}
+
+	new vortexUpdateChecker();
+
+}
