8cb784f111dd67821dc80980adab825bcb63dcc5
Vortex <124342084+webvortexTeam@users.noreply.github.com>
2024-07-01 10:44:31 +0300

    Update issue with feautured image

--- includes/unlimitedv2-class-import-activities.php
+++ includes/unlimitedv2-class-import-activities.php
@@ -1,7 +1,8 @@
 <?php
-    if (!defined('ABSPATH')) {
-        exit; // Exit if accessed directly.
-    }
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
 function unlimited_andrenaline_import_activities()
 {
     $whitelabelid = get_option('activity_host_url_label');
@@ -120,168 +121,197 @@ function unlimited_andrenaline_import_activities()
                 update_field('field_webvortex_category_ids', implode(', ', $activity['categoryIds']), $post_id);
                 update_field('field_webvortex_description', $activity['description'], $post_id);
 
-                // Photos
-                if (!empty($activity['photos'])) {
-                    $photos = array();
-                    foreach ($activity['photos'] as $photo) {
-                        $photos[] = array(
-                            'field_webvortex_photo_id' => $photo['id'],
-                            'field_webvortex_photo_full' => $photo['full'],
-                            'field_webvortex_photo_thumb' => $photo['thumb'],
-                            'field_webvortex_photo_title' => $photo['title']
-                        );
-                    }
-                    update_field('field_webvortex_photos', $photos, $post_id);
-                }
+if (!empty($activity['photos'])) {
+    $photos = array();
+    foreach ($activity['photos'] as $photo) {
+        $photos[] = array(
+            'field_webvortex_photo_id' => $photo['id'],
+            'field_webvortex_photo_full' => $photo['full'],
+            'field_webvortex_photo_thumb' => $photo['thumb'],
+            'field_webvortex_photo_title' => $photo['title']
+        );
+    }
+    update_field('field_webvortex_photos', $photos, $post_id);
+
+    // Set the featured image
+    $image_url = $photos[0]['field_webvortex_photo_full'];
+    if (!empty($image_url)) {
+        set_featured_image_from_url($image_url, $post_id);
+    }
+}
+
 
                 // Itineraries
                 if (!empty($activity['itineraries'])) {
                     $itineraries = array();
-                    foreach ($activity['itineraries'] as $itinerary) {
-                        $included = array();
-                        if (!empty($itinerary['details']['included'])) {
-                            foreach ($itinerary['details']['included'] as $item) {
-                                $included[] = array(
-                                    'field_webvortex_included_id' => $item['id'],
-                                    'field_webvortex_included_title' => $item['title']
-                                );
-                            }
-                        }
+                    foreach ($itinerary['details']['doNotForget'] as $item) {
+                    $do_not_forget[] = array(
+                        'field_webvortex_do_not_forget_id' => $item['id'],
+                        'field_webvortex_do_not_forget_title' => $item['title']
+                    );
+                }
 
-                        $do_not_forget = array();
-                        if (!empty($itinerary['details']['doNotForget'])) {
-                            foreach ($itinerary['details']['doNotForget'] as $item) {
-                                $do_not_forget[] = array(
-                                    'field_webvortex_do_not_forget_id' => $item['id'],
-                                    'field_webvortex_do_not_forget_title' => $item['title']
-                                );
-                            }
-                        }
-
-                        $ratings = array();
-                        if (!empty($itinerary['ratings'])) {
-                            foreach ($itinerary['ratings'] as $rating) {
-                                $ratings[] = array(
-                                    'field_webvortex_rating_score' => $rating['score'],
-                                    'field_webvortex_rating_text' => $rating['text'],
-                                    'field_webvortex_rating_fullname' => $rating['fullname']
-                                );
-                            }
-                        }
-
-                        $languages = array();
-                        if (!empty($itinerary['spokenLanguages'])) {
-                            foreach ($itinerary['spokenLanguages'] as $language) {
-                                $languages[] = array(
-                                    'field_webvortex_language_id' => $language['id'],
-                                    'field_webvortex_language_acrn' => $language['acrn'],
-                                    'field_webvortex_language_title' => $language['title']
-                                );
-                            }
-                        }
-
-                        $itineraries[] = array(
-                            'field_webvortex_itinerary_id' => $itinerary['id'],
-                            'field_webvortex_itinerary_title' => $itinerary['title'],
-                            'field_webvortex_itinerary_description' => $itinerary['description'],
-                            'field_webvortex_itinerary_level' => $itinerary['level'],
-                            'field_webvortex_itinerary_details' => array(
-                                'field_webvortex_itinerary_included' => $included,
-                                'field_webvortex_itinerary_do_not_forget' => $do_not_forget
-                            ),
-                            'field_webvortex_itinerary_ratings' => $ratings,
-                            'field_webvortex_itinerary_min_age' => $itinerary['min_age'],
-                            'field_webvortex_itinerary_spoken_languages' => $languages,
-                            'field_webvortex_itinerary_booking_type' => $itinerary['bookingType'],
-                            'field_webvortex_itinerary_transport' => $itinerary['transport'],
-                            'field_webvortex_itinerary_allow_multiple_facilities' => $itinerary['allowSelectionOfMultipleFacilities'],
-                            'field_webvortex_itinerary_accommodation' => $itinerary['accommodation'],
-                            'field_webvortex_itinerary_min_persons' => $itinerary['min_persons'],
-                            'field_webvortex_itinerary_min_price' => $itinerary['min_price'],
-                            'field_webvortex_cancellation_policy' => array(
-                                'field_webvortex_policy_id' => $itinerary['cancellationPolicy']['id'],
-                                'field_webvortex_policy_charge' => $itinerary['cancellationPolicy']['charge'],
-                                'field_webvortex_policy_days_before_1' => $itinerary['cancellationPolicy']['days_before_1'],
-                                'field_webvortex_policy_days_before_2' => $itinerary['cancellationPolicy']['days_before_2'],
-                                'field_webvortex_policy_title' => $itinerary['cancellationPolicy']['title'],
-                                'field_webvortex_policy_description' => $itinerary['cancellationPolicy']['description']
-                            ),
-                            'field_webvortex_itinerary_is_event' => $itinerary['isEvent'],
-                            'field_webvortex_itinerary_difficulty' => $itinerary['difficulty'],
-                            'field_webvortex_itinerary_latitude' => $itinerary['latitude'],
-                            'field_webvortex_itinerary_longitude' => $itinerary['longitude'],
-                            'field_webvortex_itinerary_duration' => $itinerary['duration']
+                $ratings = array();
+                if (!empty($itinerary['ratings'])) {
+                    foreach ($itinerary['ratings'] as $rating) {
+                        $ratings[] = array(
+                            'field_webvortex_rating_score' => $rating['score'],
+                            'field_webvortex_rating_text' => $rating['text'],
+                            'field_webvortex_rating_fullname' => $rating['fullname']
                         );
                     }
-                    update_field('field_webvortex_itineraries', $itineraries, $post_id);
                 }
 
-                $activity_categories = array();
-                foreach ($activity['categoryIds'] as $category_id) {
-                    if (isset($category_map[$category_id])) {
-                        $term = term_exists($category_map[$category_id], 'activity_category');
-                        if (!$term) {
-                            // Κεντρική κατηγορία
-                            $category_data = array_filter($categories_data, function($cat) use ($category_id) {
-                                return $cat['id'] === $category_id || in_array($category_id, array_column($cat['subcategories'], 'id'));
-                            });
-                            
-                            if ($category_data) {
-                                $category_data = array_shift($category_data);
-                                $description = $category_data['description'];
-                                
-                                // subcategory
-                                if (isset($category_map[$category_id])) {
-                                    $subcategory_data = array_filter($category_data['subcategories'], function($subcat) use ($category_id) {
-                                        return $subcat['id'] === $category_id;
-                                    });
-                                    if ($subcategory_data) {
-                                        $subcategory_data = array_shift($subcategory_data);
-                                        $description = $subcategory_data['description'];
-                                    }
-                                }
-
-                                $term = wp_insert_term(
-                                    $category_map[$category_id],
-                                    'activity_category',
-                                    array('description' => $description)
-                                );
-                            }
-                        }
-                        if (!is_wp_error($term)) {
-                            $activity_categories[] = intval($term['term_id']);
-                        }
+                $languages = array();
+                if (!empty($itinerary['spokenLanguages'])) {
+                    foreach ($itinerary['spokenLanguages'] as $language) {
+                        $languages[] = array(
+                            'field_webvortex_language_id' => $language['id'],
+                            'field_webvortex_language_acrn' => $language['acrn'],
+                            'field_webvortex_language_title' => $language['title']
+                        );
                     }
                 }
-                wp_set_post_terms($post_id, $activity_categories, 'activity_category');
 
-                $activities_imported++;
+                $itineraries[] = array(
+                    'field_webvortex_itinerary_id' => $itinerary['id'],
+                    'field_webvortex_itinerary_title' => $itinerary['title'],
+                    'field_webvortex_itinerary_description' => $itinerary['description'],
+                    'field_webvortex_itinerary_level' => $itinerary['level'],
+                    'field_webvortex_itinerary_details' => array(
+                        'field_webvortex_itinerary_included' => $included,
+                        'field_webvortex_itinerary_do_not_forget' => $do_not_forget
+                    ),
+                    'field_webvortex_itinerary_ratings' => $ratings,
+                    'field_webvortex_itinerary_min_age' => $itinerary['min_age'],
+                    'field_webvortex_itinerary_spoken_languages' => $languages,
+                    'field_webvortex_itinerary_booking_type' => $itinerary['bookingType'],
+                    'field_webvortex_itinerary_transport' => $itinerary['transport'],
+                    'field_webvortex_itinerary_allow_multiple_facilities' => $itinerary['allowSelectionOfMultipleFacilities'],
+                    'field_webvortex_itinerary_accommodation' => $itinerary['accommodation'],
+                    'field_webvortex_itinerary_min_persons' => $itinerary['min_persons'],
+                    'field_webvortex_itinerary_min_price' => $itinerary['min_price'],
+                    'field_webvortex_cancellation_policy' => array(
+                        'field_webvortex_policy_id' => $itinerary['cancellationPolicy']['id'],
+                        'field_webvortex_policy_charge' => $itinerary['cancellationPolicy']['charge'],
+                        'field_webvortex_policy_days_before_1' => $itinerary['cancellationPolicy']['days_before_1'],
+                        'field_webvortex_policy_days_before_2' => $itinerary['cancellationPolicy']['days_before_2'],
+                        'field_webvortex_policy_title' => $itinerary['cancellationPolicy']['title'],
+                        'field_webvortex_policy_description' => $itinerary['cancellationPolicy']['description']
+                    ),
+                    'field_webvortex_itinerary_is_event' => $itinerary['isEvent'],
+                    'field_webvortex_itinerary_difficulty' => $itinerary['difficulty'],
+                    'field_webvortex_itinerary_latitude' => $itinerary['latitude'],
+                    'field_webvortex_itinerary_longitude' => $itinerary['longitude'],
+                    'field_webvortex_itinerary_duration' => $itinerary['duration']
+                );
+            }
+            update_field('field_webvortex_itineraries', $itineraries, $post_id);
+        }
+
+        $activity_categories = array();
+        foreach ($activity['categoryIds'] as $category_id) {
+            if (isset($category_map[$category_id])) {
+                $term = term_exists($category_map[$category_id], 'activity_category');
+                if (!$term) {
+                    // Κεντρική κατηγορία
+                    $category_data = array_filter($categories_data, function($cat) use ($category_id) {
+                        return $cat['id'] === $category_id || in_array($category_id, array_column($cat['subcategories'], 'id'));
+                    });
+                    
+                    if ($category_data) {
+                        $category_data = array_shift($category_data);
+                        $description = $category_data['description'];
+                        
+                        // subcategory
+                        if (isset($category_map[$category_id])) {
+                            $subcategory_data = array_filter($category_data['subcategories'], function($subcat) use ($category_id) {
+                                return $subcat['id'] === $category_id;
+                            });
+                            if ($subcategory_data) {
+                                $subcategory_data = array_shift($subcategory_data);
+                                $description = $subcategory_data['description'];
+                            }
+                        }
+
+                        $term = wp_insert_term(
+                            $category_map[$category_id],
+                            'activity_category',
+                            array('description' => $description)
+                        );
+                    }
+                }
+                if (!is_wp_error($term)) {
+                    $activity_categories[] = intval($term['term_id']);
+                }
             }
         }
+        wp_set_post_terms($post_id, $activity_categories, 'activity_category');
+
+        $activities_imported++;
     }
-    $desired_permalink_structure = '/%postname%/';
-    
-    // fix issue with permanlink
-    $current_permalink_structure = get_option('permalink_structure');
-    
-    update_option('permalink_structure', $desired_permalink_structure);
-    $log = get_option( 'uac_cron_log', [] );
-    $log[] = 'Το Cron έτρεξε αυτόματα στις: ' . current_time( 'mysql' );
-    update_option( 'uac_cron_log', $log );
-    update_option('activities_imported', $activities_imported);
-    echo "<h3> Η Εισαγωγή/Ανανέωση ολοκληρώθηκε συνολικά σε: $activities_imported δραστηριότητες </h3>";
 }
+
+$desired_permalink_structure = '/%postname%/';
+
+// fix issue with permanlink
+$current_permalink_structure = get_option('permalink_structure');
+
+update_option('permalink_structure', $desired_permalink_structure);
+$log = get_option( 'uac_cron_log', [] );
+$log[] = 'Το Cron έτρεξε αυτόματα στις: ' . current_time( 'mysql' );
+update_option( 'uac_cron_log', $log );
+update_option('activities_imported', $activities_imported);
+echo "<h3> Η Εισαγωγή/Ανανέωση ολοκληρώθηκε συνολικά σε: $activities_imported δραστηριότητες </h3>";
+}
+
 function create_activity_category_taxonomy() {
-    register_taxonomy(
-        'activity_category',
-        'activity',
-        array(
-            'label' => __( 'Activity Categories' ),
-            'rewrite' => array( 'slug' => 'activity-category' ),
-            'hierarchical' => true,
-        )
-    );
+register_taxonomy(
+    'activity_category',
+    'activity',
+    array(
+        'label' => __( 'Activity Categories' ),
+        'rewrite' => array( 'slug' => 'activity-category' ),
+        'hierarchical' => true,
+    )
+);
 }
-add_action( 'init', 'create_activity_category_taxonomy' );
+
+function set_featured_image_from_url($image_url, $post_id)
+{
+    require_once(ABSPATH . 'wp-admin/includes/file.php');
+    require_once(ABSPATH . 'wp-admin/includes/media.php');
+    require_once(ABSPATH . 'wp-admin/includes/image.php');
+
+    $tmp = download_url($image_url);
+
+    if (is_wp_error($tmp)) {
+        return false;
+    }
+
+    $file_array = array(
+        'name' => basename($image_url),
+        'tmp_name' => $tmp
+    );
+
+    if (is_wp_error($file_array['tmp_name'])) {
+        @unlink($file_array['tmp_name']);
+        return false;
+    }
+
+    $attach_id = media_handle_sideload($file_array, $post_id);
+
+    if (is_wp_error($attach_id)) {
+        @unlink($file_array['tmp_name']);
+        return false;
+    }
+
+    set_post_thumbnail($post_id, $attach_id);
+
+    return true;
+}
+
+
+add_action('init', 'create_activity_category_taxonomy');
 
 ?>
