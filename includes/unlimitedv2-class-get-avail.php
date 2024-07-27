55073ecf89506850931d160a374f603b6de13971
Vortex <124342084+webvortexTeam@users.noreply.github.com>
2024-07-27 13:42:59 +0300

    fix past-time issue

--- includes/unlimitedv2-class-get-avail.php
+++ includes/unlimitedv2-class-get-avail.php
@@ -9,9 +9,10 @@
     
         $itinerary_id = sanitize_text_field($_POST['itinerary_id']);
         
-        // Set default start and end dates
-        $start_date = isset($_POST['start_date']) ? sanitize_text_field($_POST['start_date']) : date('Y-m-d');
-        $end_date = isset($_POST['end_date']) ? sanitize_text_field($_POST['end_date']) : date('Y-m-d', strtotime('+30 days'));
+        date_default_timezone_set('Europe/Athens'); // Greece
+        $current_time = date('H:i');
+        $start_date = isset($_POST['start_date']) ? sanitize_text_field($_POST['start_date']) . ' ' . $current_time : date('Y-m-d') . ' ' . $current_time;
+        $end_date = isset($_POST['end_date']) ? sanitize_text_field($_POST['end_date']) . ' 23:59' : date('Y-m-d', strtotime('+30 days')) . ' 23:59';
     
         $availability = unlimited_adrenaline_remote_call(
             'get',
@@ -33,5 +34,4 @@
     
     add_action('wp_ajax_get_availability', 'get_availability');
     add_action('wp_ajax_nopriv_get_availability', 'get_availability');
-    
-    
+?>
